de3fb0fe1c58b4ec97cbaf1061108dfbe3196729
Tyler Machen <tylermachen@gmail.com>
2015-07-30 21:43:31 -0400

    complete oxford_comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  end
+  array.map { |elem| array.last == elem ? "and #{elem}" : elem }.join(', ')
 end
